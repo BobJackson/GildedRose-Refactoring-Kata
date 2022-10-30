c73f46bc0a624ae9cc6fddedff9124c7407be244
Bob Jackson <1187688895@qq.com>
2022-10-30 14:38:47 +0800

    refactor: use factory method to create item object

--- Java/src/test/java/com/gildedrose/TextTestFixture.java
+++ Java/src/test/java/com/gildedrose/TextTestFixture.java
@@ -3,6 +3,8 @@ package com.gildedrose;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static com.gildedrose.Item.*;
+
 public class TextTestFixture {
 
     public static String getBaseline() {
@@ -10,17 +12,17 @@ public class TextTestFixture {
         PrintStream printStream = new PrintStream(out);
         printStream.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+        Item[] items = new Item[]{
+            createNormal("+5 Dexterity Vest", 10, 20), //
+            createAgedBrie(2, 0), //
+            createNormal("Elixir of the Mongoose", 5, 7), //
+            createSulfuras(0, 80), //
+            createSulfuras(-1, 80),
+            createBackstagePass(15, 20),
+            createBackstagePass(10, 49),
+            createBackstagePass(5, 49),
+            // this conjured item does not work properly yet
+            createNormal("Conjured Mana Cake", 3, 6)};
 
         GildedRose app = new GildedRose(items);
 
